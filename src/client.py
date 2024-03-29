3ebdd9580b3cdb0c49abda349586463a9acddb0c
Jaydin_MacBook <74679492+TheManWhoLikesToCode@users.noreply.github.com>
2024-03-29 15:23:18 -0400

    Added reading function

--- src/client.py
+++ src/client.py
@@ -1,33 +1,34 @@
+"""
+Author: Jaydin F
+Date: 03/29/2024
+Description: Client
+
+"""
+
 import os
-import requests
-import base64
-import json
 import speech_recognition as sr
 from gtts import gTTS
 from dotenv import load_dotenv
-
+import google.generativeai as genai
+from tools import read
+from config import generation_config, safety_settings
 
 load_dotenv()
 
-GOOGLE_API_KEY = os.environ.get("AUTH_TOKEN")
-API_URL = os.environ.get("API_URL")
-
-
-def send_request(endpoint, data=None, files=None):
-    headers = {"Authorization": f"Bearer {GOOGLE_API_KEY}"}
-    if files:
-        response = requests.post(
-            f"{API_URL}/{endpoint}", data=data, files=files, headers=headers
-        )
-    else:
-        response = requests.post(
-            f"{API_URL}/{endpoint}", data=data, headers=headers
-        )
-    return response.json()
+GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
 
 
 def speak(text):
-    tts = gTTS(text=text, lang='en')
+    """
+    Converts the given text to speech and plays it.
+
+    Args:
+        text (str): The text to be converted to speech.
+
+    Returns:
+        None
+    """
+    tts = gTTS(text=text, lang="en")
     tts.save("output.mp3")
     # Use 'afplay' for macOS, 'mpg321' for Linux, or 'start' for Windows
     os.system("afplay output.mp3")
@@ -35,6 +36,16 @@ def speak(text):
 
 
 def listen():
+    """
+    Listens to audio input from the microphone and uses speech recognition to convert it into text.
+
+    Returns:
+        str: The recognized text from the audio input.
+
+    Raises:
+        sr.UnknownValueError: If the speech recognition engine could not understand the audio input.
+        sr.RequestError: If there was an error making the request to the speech recognition service.
+    """
     recognizer = sr.Recognizer()
     recognizer.energy_threshold = 300  # Adjust the value as needed
     recognizer.pause_threshold = 1.0  # Adjust the value as needed
@@ -47,47 +58,52 @@ def listen():
     try:
         print("Recognizing...")
         text = recognizer.recognize_google(audio)
-        print(f"User: {text}")
         return text
     except sr.UnknownValueError as e:
         print(f"UnknownValueError: {str(e)}")
-        return ""
+        return None
     except sr.RequestError as e:
         print(f"RequestError: {str(e)}")
-        return ""
+        return None
 
 
 def main():
+    """
+    Main function that serves as the entry point of the program.
+    It loads the model, listens for user input, generates a response,
+    and handles image uploads if prompted.
+
+    Returns:
+        None
+    """
+    genai.configure(api_key=GOOGLE_API_KEY)
+
+    # Load Model
+    model = genai.GenerativeModel(
+        model_name="gemini-pro",
+        generation_config=generation_config,
+        safety_settings=safety_settings,
+        tools=[read],
+    )
+    chat = model.start_chat(enable_automatic_function_calling=True)
+
     while True:
         user_input = listen()
+        if user_input is None:
+            speak("Sorry, I did not understand that. Please try again.")
+            continue
         if user_input.lower() == "exit":
             speak("Goodbye!")
             break
 
-        data = {"prompt": user_input}
-        response = send_request(f"conversation?model=gemini-pro", data=data)
+        # Generate the response using the model with function calling
+        response = chat.send_message(user_input)
+        speak(response.text)
 
-        if "message" in response:
-            speak(response["message"])
-            image_path = input("Enter the path to the image: ")
-            with open(image_path, "rb") as image_file:
-                base64_image = base64.b64encode(
-                    image_file.read()).decode("utf-8")
-            mode = "reading_mode" if "reading" in response["message"].lower(
-            ) else "magnifying_mode"
-            files = {"file": (image_path, base64_image)}
-            data = {"mode": mode}
-            response = send_request("capture_image", data=data, files=files)
-            if "text" in response:
-                speak("Extracted Text:")
-                speak(response["text"])
-            elif "description" in response:
-                speak("Detailed Description:")
-                speak(response["description"])
-        elif "response" in response:
-            speak("Assistant: " + response["response"])
-        else:
-            speak("Sorry, an error occurred.")
+        for content in chat.history:
+            part = content.parts[0]
+            print(content.role, "->", type(part).to_dict(part))
+            print("-" * 80)
 
 
 if __name__ == "__main__":
