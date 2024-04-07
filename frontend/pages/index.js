6dc6ddd6a4727826e6ddd08b6df2753dd31a9a11
Jaydin_MacBook <74679492+TheManWhoLikesToCode@users.noreply.github.com>
2024-04-07 10:02:02 -0400

    Added About Me Section

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -50,6 +50,10 @@ export default function Home() {
         <img src="/logo.svg" alt="Logo" className={styles.logoImage} />
       </div>
 
+      <div className={styles.header}>
+        <a href='/about' className={styles.aboutLink}>About us</a>
+      </div>
+
       <Head>
         <title>Demo Page</title>
         <link rel="icon" href="/favicon.ico" />
@@ -104,54 +108,6 @@ export default function Home() {
           <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
         </a>
       </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        .video {
-          width: 100%;
-          max-width: 640px;
-          height: auto;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
     </div>
   );
 }
